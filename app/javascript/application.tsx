3bcc85747d87d17f089c60646f14e0b8686c950a
David Bindloss <d.bindloss@gmail.com>
2022-01-21 08:07:26 +1100

    Starting work on FE application.

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,1 +1,13 @@
 // Entry point for the build script in your package.json
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+// import bootstrap js/css
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from "./components/home";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Home />, document.getElementById("app"));
+});
---
+++ app/javascript/components/home.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <main>
+      <div className="px-4 py-5 my-5 text-center">
+        <h1 className="display-5 fw-bold">2Kb Collective</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">Coding Courses, Tutorials and More</p>
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center"></div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
